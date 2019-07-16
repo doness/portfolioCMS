6e25e3e38238d7a968228bd01ce7e958eddab0da
Doness <doness@users.noreply.github.com>
2019-07-16 13:21:02 +0700

    Update db_conn.php

--- db/dbconn.php
+++ db/dbconn.php
@@ -1,6 +1,6 @@
 <?php
-$db_servername = 'localhost';
-$db_username = 'root';
-$db_password = 'r00t';
-$db_name = 'portfolioCMS';
+$db_servername = 't89yihg12rw77y6f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	';
+$db_username = 'lwmh8qwvehjrsspm';
+$db_password = 'baegzpwp1rsxum5t';
+$db_name = 'mp58550xk9i3eed9';
 ?>
