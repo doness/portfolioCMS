9489fe5c858e69a90dc139b0514c7a70c34e0fb1
Doness <doness@users.noreply.github.com>
2019-07-16 13:21:13 +0700

    Update dbconn.php

--- db/dbconn.php
+++ db/dbconn.php
@@ -1,5 +1,5 @@
 <?php
-$db_servername = 't89yihg12rw77y6f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	';
+$db_servername = 't89yihg12rw77y6f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $db_username = 'lwmh8qwvehjrsspm';
 $db_password = 'baegzpwp1rsxum5t';
 $db_name = 'mp58550xk9i3eed9';
